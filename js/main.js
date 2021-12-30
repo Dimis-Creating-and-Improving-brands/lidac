b973f6b221ee32db3650abb30f975097dea67557
lav djuric <liondj007@gmail.com>
2021-12-30 04:31:33 +0100

    Initial push

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const positioned = [...document.querySelectorAll('.positioned-block')];
+
+
+const onResize = _ => {
+    const positionedHeightSum = positioned.reduce((acc, curr) => acc + curr.clientHeight, 0);
+    document.querySelector('.scrollable-content').style.height = `${positionedHeightSum}px`;
+}
+
+const onScroll = _ => {
+    const bottomY = window.scrollY + window.innerHeight;
+    if (bottomY - 50 > positioned.length * window.innerHeight) {
+        positioned.forEach(block => {
+            block.style.position = `static`;
+        })
+    } else {
+        positioned.forEach((block, index) => {
+            const height = block.clientHeight;
+            const opacity = ((height * (index + 1)) - scrollY) / height;
+            if (opacity <= 1.2 && opacity >= 0.9) block.classList.add('animated');
+            block.style.position = `fixed`;
+            block.style.opacity = `${opacity}`;
+        })
+    }
+}
+
+
+window.addEventListener('scroll', onScroll);
+window.addEventListener('resize', onResize);
+
+onScroll();
+onResize();
